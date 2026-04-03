6876829e14db481248bc2dedbe7c01feea949df2
gaiadrd <onaircoc@gmail.com>
2026-04-03 22:19:04 +0200

    before the big merge (on our way to leaf)

--- src/MainLoop/ExecutionContext.ts
+++ src/MainLoop/ExecutionContext.ts
@@ -34,7 +34,7 @@ export class ExecutionContext {
             case "arrival":
                 this.robot.posListeners.push((pos: GridPoint) => {
                     if (GridUtils.equals(pos, goal.args.flagPos)) {
-                        this.scene.nextIsland();
+                        this.scene.nextLeaf();
                     }
                 });
                 break;
--- src/MainLoop/Scene/PlayScene.ts
+++ src/MainLoop/Scene/PlayScene.ts
@@ -1,20 +1,19 @@
-import { KeyboardEventTypes, ThinBlackAndWhitePostProcess } from "@babylonjs/core";
-import { ArcRotateCamera, Engine, HemisphericLight, Vector3, Viewport } from "@babylonjs/core";
-import { GameScene } from "./GameScene";
+import { ArcRotateCamera, Engine, HemisphericLight, KeyboardEventTypes, Vector3, Viewport } from "@babylonjs/core";
 import { Level } from "../../Environment/Level";
-import { LevelReader, State } from "../../Environment/LevelReader";
-import { LayerMasks } from "../../Shared/Constants";
-import { StartButton } from "../../MRGUI/buttons/StartButton";
-import { GridUtils } from "../../Shared/GridUtils";
-import { ExecutionContext, type Goal } from "../ExecutionContext";
-import { StackPanel } from "@babylonjs/gui";
+import { LevelReader, State, type IslandMap } from "../../Environment/LevelReader";
 import { QuitButton } from "../../MRGUI/buttons/QuitButton";
+import { StartButton } from "../../MRGUI/buttons/StartButton";
+import { LayerMasks } from "../../Shared/Constants";
+import { ExecutionContext } from "../ExecutionContext";
+import { GameScene } from "./GameScene";
 //import { Player } from "../entities/Player";
 
 export class PlayScene extends GameScene {
     //private player: Player;
     private levelReader: LevelReader;
     private currentIsland: number = 0;
+    private currentIslandMap: IslandMap;
+    private currentLeaf: number = 0;
 
     private ctx: ExecutionContext;
     private uiCamera: ArcRotateCamera;
@@ -48,9 +47,15 @@ export class PlayScene extends GameScene {
         this.scene.onKeyboardObservable.add((kbInfo) =>{
             console.log("key event", kbInfo.event.key);
             if (kbInfo.type == KeyboardEventTypes.KEYUP) {
-                if (kbInfo.event.key == "ArrowRight") {
+                if (kbInfo.event.key === "ArrowRight") {
                     console.log("left");
                     this.nextIsland();
+                } else if (kbInfo.event.key === "l") {
+                    console.log("nextleaf");
+                    this.nextLeaf();
+                } else if (kbInfo.event.key === "p") {
+                    console.log("prevleaf");
+                    this.previousLeaf();
                 }
             }
         });
@@ -88,22 +93,18 @@ export class PlayScene extends GameScene {
         this.scene.getEngine().hideLoadingUI();
     }
 
-    public loadIsland(index: number) {
-        const island = this.levelReader.getIsland(index);
-        if (!island || island.length === 0) {
-            throw new Error("Island not found or empty");
-        }
-        this.currentIsland = index;
+    public loadLeaf(index: number) {
+        this.currentLeaf = index;
 
         if (this.level) this.level.dispose();
 
-        const map = island[0];
+        const map = this.currentIslandMap[index];
         this.level = new Level(map, this._drh, this.scene);
 
         if (this.ctx) this.ctx.newLevel(this.level.getRobot());
         else this.ctx = new ExecutionContext(this.level.getRobot(), this);
 
-        const new_goal = this.levelReader.getGoal(index);
+        const new_goal = this.levelReader.getGoal(this.currentIsland);
         switch (new_goal.name) {
             case "arrival" :
                 const flagPos = this.level.findStatePos(State.Flag);
@@ -116,8 +117,38 @@ export class PlayScene extends GameScene {
                 throw new Error("Goal Inconnu");
         }
         this.ctx.setGoal(new_goal);
+    }
+
+    public nextLeaf() {
+        const next = this.currentLeaf + 1;
+
+        if (next >= this.currentIslandMap.length) {
+            console.log("Dernière feuille atteinte");
+            this.nextIsland();
+            return;
+        }
+
+        this.loadLeaf(next);
+    }
+
+    public previousLeaf() {
+        const prev = this.currentLeaf - 1;
+        if (prev < 0) return;
+        this.loadLeaf(prev);
+    }
+
+    public loadIsland(index: number) {
+        const island = this.levelReader.getIsland(index);
+        if (!island || island.length === 0) {
+            throw new Error("Island not found or empty");
+        }
+        this.currentIsland = index;
+        this.currentIslandMap = island;
+
+        this.loadLeaf(0);
 
         this.toolbox.clear();
+        this.ctx = new ExecutionContext(this.level.getRobot(), this);
         this.levelReader.setupToolbox(index, this.toolbox, this.ctx, this);
     }
 
