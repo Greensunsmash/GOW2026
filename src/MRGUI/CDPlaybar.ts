37a838e9d05ea78acf880b6d61c3f9eb7a36f85e
gaiadrd <onaircoc@gmail.com>
2026-04-07 01:08:47 +0200

    la saint valentin est une fete commerciale

--- src/MRGUI/CDPlaybar.ts
+++ src/MRGUI/CDPlaybar.ts
@@ -1,12 +1,14 @@
 // CD c'est pour la nostalge
 // Playbar c'est pour barre de lecture
 
-import { Container, Control, StackPanel } from "@babylonjs/gui";
+import { Container, Control, Rectangle, StackPanel } from "@babylonjs/gui";
 import { BaseButton } from "./buttons/BaseButton";
 import { BaseVSpacer } from "./misc/BaseSpacers";
 
 // Une lecture ? Pas de pb, j'ai la barre
-export class CDPlaybar extends StackPanel {
+export class CDPlaybar extends Rectangle {
+    private panel: StackPanel;
+
     constructor(
         root: Container,
         onPrev: () => void,
@@ -17,20 +19,30 @@ export class CDPlaybar extends StackPanel {
         onFullAttempt: () => void
     ) {
         super("playbar");
+
+        this.width = "200px";
+        this.height = "300px";
+        //this.adaptHeightToChildren = true;
+        //this.adaptWidthToChildren = true;
+
+        this.panel = new StackPanel();
         this.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
         this.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
 
-        this.left = "-20px"; 
-        this.top = "-20px";
+        this.panel.left = "-20px"; 
+        this.panel.top = "-20px";
 
-        this.zIndex = 100;
+        //this.panel.zIndex = 100;
 
-        this.addControl(new BaseButton("prev", "Reculer", () => onPrev()));
-        this.addControl(new BaseButton("next", "Avancer", () => onNext()));
-        this.addControl(new BaseButton("fullattempt", "Vrai essai", () => onFullAttempt()));
-        this.addControl(new BaseVSpacer());
+        this.panel.addControl(new BaseButton("prev", "Reculer", () => onPrev()));
+        this.panel.addControl(new BaseButton("next", "Avancer", () => onNext()));
+        this.panel.addControl(new BaseButton("fullattempt", "Vrai essai", () => onFullAttempt()));
+        this.panel.addControl(new BaseVSpacer());
 
-        this.addControl(new BaseButton("prevleaf", "- feuille", () => onPrevLeaf()));
-        this.addControl(new BaseButton("nextleaf", "+ feuille", () => onNextLeaf()));
+        this.panel.addControl(new BaseButton("prevleaf", "- feuille", () => onPrevLeaf()));
+        this.panel.addControl(new BaseButton("nextleaf", "+ feuille", () => onNextLeaf()));
+        this.panel.addControl(new BaseButton("dryattempt", "Faux essai", () => onDryAttempt()));
+
+        this.addControl(this.panel);
     }
 }
