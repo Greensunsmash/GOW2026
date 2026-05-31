0d1b002425cded302ddbe08ec44118d69778493f
Yoko <eloi.rathgeber@gmail.com>
2026-05-31 18:36:37 +0200

    pas

--- src/Containers/Prefabs/NotContainer.ts
+++ src/Containers/Prefabs/NotContainer.ts
@@ -8,7 +8,7 @@ import { isValuable } from "../Valuable";
 export class NotContainer extends BooleenContainer {
 
     constructor(root: Container, content_root:Container, scene: GameScene) {
-        super(["Non", "b"], root, content_root, scene);
+        super(["Pas", "b"], root, content_root, scene);
         this.shortName = "not";
     }
 
